0d93238b9dad7f3fc87bc20df035612f23b4ee12
AktharAnsar <108989168+AktharAnsar@users.noreply.github.com>
2022-07-13 08:47:42 +0530

    Update ChangeOddIndexToUpperCase.java

--- week1/day2/assignments/mandatory/ChangeOddIndexToUpperCase.java
+++ week1/day2/assignments/mandatory/ChangeOddIndexToUpperCase.java
@@ -19,4 +19,24 @@ public class ChangeOddIndexToUpperCase {
 	 * d)within the loop, change the character to uppercase, if the index is odd else don't change
 	  
 	 */
+	
+		String test = "changeme";
+		
+		char[] ch1= test.toCharArray();
+		
+		for (int i = 0; i < ch1.length; i++) {
+			
+			int j = i%2;
+			
+			if(j!=0) {
+				
+				ch1[i] = Character.toUpperCase(ch1[i]);
+			}
+			
+			System.out.print(ch1[i]);
+			
+		}
+		
+		
+	}
 }
